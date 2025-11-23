f878b541fd4fb46be5eb80c2df77d4f41b70ec85
me <cokevore@gmail.com>
2025-11-22 20:20:04 -0800

    finished TODOS

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -7,36 +7,60 @@ const JWT_SECRET = "secret";
 export default {
   async register({ username, password, profilePicture }) {
     // TODO: get ahold of the db using readDb();
+    let db = await readDb()
+    console.log(db);
+
     // TODO: check if there is an existing user with the same username
+    let isFound = db.users.find((x) => x.username == username)
+
+
     // TODO: if there is, do the following:
     //       - construct a new Error("Username already taken");
     //       - set the statusCode of that error object to 400
     //       - throw the err
+    if (isFound) {
+      let err = new Error("Username already taken");
+      err.code = 400
+      throw err
+    }
+
     // TODO: otherwise, create a user object. A user has:
     //       - id: a random string-based id (crypto.randomUUID())
     //       - username: a username
     //       - password: a password
     //       - profilePicture: their profile pic string or an empty string if no picture.
+    let user = { id: crypto.randomUUID(), username, password, profilePicture: profilePicture || "" }
+
     // TODO:  push this user object into db.users
+    db.users.push(user)
     // TODO:  call the writeDb(db) operation to save changes.
+    await writeDb(db)
     // TODO:  return the user object but without their password  (only id, username, profilePicture)
 
-    return {
-      id: "dummy-id",
-      username: "dummy-username",
-      profilePicture: "",
-    };
+    delete user.password
+
+    return user
   },
 
   async login({ username, password }) {
     // TODO: get ahold of the db using readDb();
+
+    let db = await readDb()
     // TODO: check the database for a user with a matching username and password
+    let isFound = db.users.find((x) => x.username == username && x.password == password)
     // TODO: if there is no user:
     //       - construct a new Error("Invalid username or password");
     //       - set the statusCode of that error object to 401
     //       - throw the err
+    if (!isFound) {
+      let err = new Error("Invalid username or password");
+      err.code = 401
+      throw err
+    }
+
     // TODO: otherwise, create a login token. I'll help you out with this one:
-    // const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1h" })
+    // Thank you
+    const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1h" })
     // TODO:  return an object that contains 2 things:
     //  - token
     //  - user : { id: user.id, username: user.username, profilePicture: user.profilePicture }
@@ -44,9 +68,9 @@ export default {
     return {
       token,
       user: {
-        id: "dummy-id",
-        username: "dummy-username",
-        profilePicture: "dummy-profilePicture",
+        id: isFound.id,
+        username: isFound.username,
+        profilePicture: isFound.profilePicture,
       },
     };
   },
--- backend/src/services/tipService.js
+++ backend/src/services/tipService.js
@@ -4,30 +4,60 @@ import crypto from "node:crypto";
 export default {
   async findAll() {
     // TODO: get ahold of the db using readDb();
+    let db = await readDb()
+
     // TODO: return the tips from the db
+    return db.tips
   },
 
   async create({ title, userId }) {
     // TODO: get ahold of the db using readDb();
+    let db = await readDb()
+
     // TODO: create a tip object containing { id: "some-random-id", title, userId }
+    let tip = { id: crypto.randomUUID(), title, userId }
     // TODO: push the tip object into tips list in the database
+    db.tips.push(tip)
     // TODO: write changes to database with await writeDb(db)
+    await writeDb(db)
     // TODO: return the id of the created tip
+    return tip.id
   },
 
   async update({ id, title, userId }) {
     // TODO: get ahold of the db using readDb();
+    let db = await readDb()
+
     // TODO: find a tip in the db whose id & userId matches the incoming id & userId
+    let tipIndex = db.tips.findIndex((x) => x.id == id && x.userId == userId)
     // TODO: if there is no matching tip, return false.
+    if (tipIndex) {
+      db.tips[tipIndex].title = title
+
+      await writeDb(db)
+      return true
+    }
+    return false
     // TODO: otherwise, set the found tip's title to the incoming title
+
     // TODO: write changes to database with await writeDb(db)
     // TODO: return true
   },
 
   async remove({ id, userId }) {
     // TODO: get ahold of the db using readDb();
+    let db = await readDb()
+
     // TODO: find the INDEX of the tip in the db whose id & userId match the incoming id & userId
+    let tipIndex = db.tips.findIndex((x) => x.id == id && x.userId == userId)
+
     // TODO: if there is no index (-1), return false.
+    if (tipIndex) {
+      db.tips.splice(tipIndex, 1)
+      await writeDb(db)
+      return true
+    }
+    return false
     // TODO: otherwise, use splice to delete from db.tips the tip based on the index
     // TODO: write changes to database with await writeDb(db)
     // TODO: return true
